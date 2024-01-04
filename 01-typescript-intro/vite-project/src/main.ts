f062a81d0ce291097c279c2a6e066907e2cf21d4
Angel Zarza <angel.zarza@reclamador.es>
2024-01-04 14:17:01 +0100

    feat: first lesson complete

--- 01-typescript-intro/vite-project/src/main.ts
+++ 01-typescript-intro/vite-project/src/main.ts
@@ -1,3 +1,10 @@
 import "./style.css"
+import {name, hpPoints, isAlive} from "./topics/01-basic-types"
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `Vite app`
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <h1>Vite app</h1>
+  </br>
+  <p>name: ${name}</p>
+  <p>hpPoints: ${hpPoints}</p>
+  <p>isAlive: ${isAlive}</p>
+`
---
+++ 01-typescript-intro/vite-project/src/topics/01-basic-types.ts
@@ -0,0 +1,9 @@
+const name = "Angel"
+let hpPoints: number | "FULL" = 10
+const isAlive = true
+
+hpPoints = "FULL"
+
+console.log({"name": name, "hpPoints": hpPoints, "isAlive": isAlive})
+
+export {name, hpPoints, isAlive}
