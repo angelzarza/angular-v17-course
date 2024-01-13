9da83593a004437f596d20efff5e7a7752c7fb78
Angel Zarza <angel.zarza@reclamador.es>
2024-01-13 14:33:55 +0100

    refactor: add extend class

--- 01-typescript-intro/vite-project/src/topics/08-classes.ts
+++ 01-typescript-intro/vite-project/src/topics/08-classes.ts
@@ -11,4 +11,16 @@ class Person {
 const person = new Person("Angel", "Spain")
 console.table({ person })
 
+export class Hero extends Person {
+  power: string
+
+  constructor(name: string, country: string, power: string) {
+    super(name, country)
+    this.power = power
+  }
+}
+
+const hero = new Hero("Loki", "Asghard", "Mimic")
+console.table({ hero })
+
 export const { name, country } = person
