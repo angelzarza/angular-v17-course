884e3cae0a1662ee30031aba6d7375e7a846a97b
Angel Zarza <angel.zarza@reclamador.es>
2024-01-21 19:22:41 +0100

    refactor: remove class inheritance

--- 01-typescript-intro/vite-project/src/topics/08-classes.ts
+++ 01-typescript-intro/vite-project/src/topics/08-classes.ts
@@ -23,4 +23,26 @@ export class Hero extends Person {
 const hero = new Hero("Loki", "Asghard", "Mimic")
 console.table({ hero })
 
+export class OtherHero {
+  name: string
+  country: string
+  power: string
+
+  constructor(
+    name: string,
+    country: string,
+    power: string,
+    public person: Person
+  ) {
+    this.name = name
+    this.country = country
+    this.power = power
+  }
+}
+
+export const personTwo = new Person("Thor", "Ashgard")
+export const heroTwo = new OtherHero("Loki", "Asghard", "Mimic", personTwo)
+console.table({ personTwo })
+console.table({ heroTwo })
+
 export const { name, country } = person
