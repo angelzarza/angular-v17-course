0393aca9e04165c6d81a764bec50560111f0d3b8
Angel Zarza <angel.zarza@reclamador.es>
2024-02-19 20:36:00 +0100

    feat: add hero base content and new flag for ng serve

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
-
+  public name: string = 'ironman';
+  public age: number = 32;
 }
