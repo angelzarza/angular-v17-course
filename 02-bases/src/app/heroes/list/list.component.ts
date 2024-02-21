1d6cdb5a502893cc34a7212e1560a761789f6e46
Angel Zarza <angel.zarza@reclamador.es>
2024-02-21 20:42:17 +0100

    feat: use ngIf else and ngFor

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  public listHeroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+  public heroInfo?: string;
 
+  removeLastHero(): void {
+    this.heroInfo = this.listHeroes.pop();
+  }
 }
