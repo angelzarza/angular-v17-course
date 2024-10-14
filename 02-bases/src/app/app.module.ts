24fea595bf50d5b6d297f6aa751cb77f9488fb87
Angel Zarza <angel.zarza@reclamador.es>
2024-10-14 21:56:12 +0200

    feat: add new dbz module

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
 
-import { CounterModule } from './counter/components/counter.module';
-import { HeroesModule } from './heroes/components/heroes.module';
+import {DbzModule} from "./dbz/dbz.module";
 
-import { AppComponent } from './app.component';
+import {CounterModule} from './counter/components/counter.module';
+import {HeroesModule} from './heroes/components/heroes.module';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+
+export class AppModule {
+}
